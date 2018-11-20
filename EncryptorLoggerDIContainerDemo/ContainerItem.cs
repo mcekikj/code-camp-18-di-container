741568f2573d85bc192b082036011d12cb3723a5
Miodrag Cekikj <miodrag@f5tech.mk>
2018-11-20 23:20:56 +0100

    Add project files.

---
+++ EncryptorLoggerDIContainerDemo/ContainerItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EncryptorLoggerDIContainerDemo
+{
+	public class ContainerItem
+	{
+		public Type ConcreteType { get; set; }
+
+		public Type AbstractionType { get; set; }
+	}
+}
